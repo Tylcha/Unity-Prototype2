718e3aeaa8e123b5d5b9fa06e75a968b6a5ea13f
Talha Nazli <ttalhanazli@gmail.com>
2023-06-16 21:50:49 +0300

    +add randomly animals spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -16,6 +16,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //randomly 0 to gameobject lenght
+        int animalIndex = Random.Range(0, animalPrefebs.Length);
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefebs[animalIndex], new Vector3(0,0,20), animalPrefebs[animalIndex].transform.rotation);
+        }
     }
 }
