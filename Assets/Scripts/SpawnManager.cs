bce9e06645bcbd1daab04c03aaa7fdb6c3a433a1
Talha Nazli <ttalhanazli@gmail.com>
2023-06-16 22:39:16 +0300

    + randomly spawn location

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,9 @@ public class SpawnManager : MonoBehaviour
     //animals array
     public GameObject[] animalPrefebs;
 
+    private float spawnRageX = 20;
+    private float spawnPositionZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +21,12 @@ public class SpawnManager : MonoBehaviour
     {
         //randomly 0 to gameobject lenght
         int animalIndex = Random.Range(0, animalPrefebs.Length);
+        //randomly spawn animal
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRageX, spawnRageX), 0, spawnPositionZ);
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefebs[animalIndex], new Vector3(0,0,20), animalPrefebs[animalIndex].transform.rotation);
+            Instantiate(animalPrefebs[animalIndex], spawnPos, animalPrefebs[animalIndex].transform.rotation);
         }
     }
 }
