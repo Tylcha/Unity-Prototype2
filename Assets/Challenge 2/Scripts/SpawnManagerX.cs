50e5bbb9d74df380df0b4c29a20b896233c6d5c2
Talha Nazli <ttalhanazli@gmail.com>
2023-06-18 01:29:21 +0300

    +add  spawn interval a random value between 3 seconds and 5 seconds: challege 2 mission8

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,17 +10,19 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    //private float startDelay = 1.0f;
+    //private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnSec());
+
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
@@ -29,4 +31,17 @@ public class SpawnManagerX : MonoBehaviour
         Instantiate(ballPrefabs[randomBallIndex], spawnPos, ballPrefabs[randomBallIndex].transform.rotation);
     }
 
+    //random 3 to 6 wait spawnball
+    IEnumerator SpawnSec()
+    {
+        //while true because everytime run otherwise one time worked
+        while (true)
+        {
+            float randomWaitSec = Random.Range(3, 6);
+            yield return new WaitForSeconds(randomWaitSec);
+            Debug.Log(randomWaitSec);
+            SpawnRandomBall();
+        }
+
+    }
 }
