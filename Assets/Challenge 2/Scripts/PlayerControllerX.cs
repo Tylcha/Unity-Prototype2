0f227f03b6048dc9d293073bd00b3c845ead8365
Talha Nazli <ttalhanazli@gmail.com>
2023-06-18 00:52:40 +0300

    +add dog 1 second timer spawn

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    private float dogSpawCooldown = 1;
+    private float timer = 0;
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog, start else timer, 1 second delay
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= dogSpawCooldown)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0;
+        }
+        else
+        {
+            timer += Time.deltaTime;
         }
     }
 }
