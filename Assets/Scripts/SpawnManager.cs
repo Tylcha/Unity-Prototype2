70fc81f1859c15444faf9157a073b2951d4aaaef
Talha Nazli <ttalhanazli@gmail.com>
2023-06-18 04:59:54 +0300

    +add animal spawn z direction with rotation

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -18,6 +18,7 @@ public class SpawnManager : MonoBehaviour
     {
         //start 2 sec later, 1.5 sec spawn
         InvokeRepeating("SpawnRandomlyAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAgressiveAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -34,5 +35,42 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRageX, spawnRageX), 0, spawnPositionZ);
 
         Instantiate(animalPrefebs[animalIndex], spawnPos, animalPrefebs[animalIndex].transform.rotation);
+
+        
+
+    }
+    void SpawnAgressiveAnimal()
+    {
+        float xPositive = 25;
+        float xNegative = -25;
+        
+        float zMaxRange = 16;
+        float zMinRange = 6;
+        float zRandomRange = Random.Range(zMaxRange,zMinRange);
+
+        float yPositiveDegress = 90;
+        float yegativeDegress = -90;
+
+        //randomly 0 to gameobject lenght
+        int animalIndex = Random.Range(0, animalPrefebs.Length);
+        
+        //static x position
+        float[] xRandom = { -25, 25 };
+
+        //<--------------------------------------------------------------------->
+
+        //RandomZ area, x-25
+        Vector3 SpawnPosAgrressiveXPositive = new Vector3(xNegative, 0, zRandomRange);
+        
+        //90 degress
+        Instantiate(animalPrefebs[animalIndex], SpawnPosAgrressiveXPositive, Quaternion.Euler(0,yPositiveDegress,0));
+
+        //<---------------------------------------------------------------------->
+
+        //RandomZ area, x-25
+        Vector3 SpawnPosAgrressiveXNegative = new Vector3(xPositive, 0, zRandomRange);
+
+        //-90 deggress
+        Instantiate(animalPrefebs[animalIndex], SpawnPosAgrressiveXNegative, Quaternion.Euler(0, yegativeDegress, 0));
     }
 }
